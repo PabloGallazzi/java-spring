b79cd1b12fd582f00ed023aa45c5b34da3d2c8ca
niko118 <nicolas.garcia@hotmail.com.ar>
2016-04-11 18:28:22 -0300

    Running tests with test profile.

--- src/test/java/spring/BaseTester.java
+++ src/test/java/spring/BaseTester.java
@@ -4,6 +4,7 @@ import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
@@ -17,6 +18,7 @@ import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppC
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = Application.class)
 @WebAppConfiguration
+@ActiveProfiles("test")
 public class BaseTester {
 
     protected MockMvc mockMvc;
