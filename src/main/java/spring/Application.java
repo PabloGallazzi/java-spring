589ff9c108e6e9aa113ee0d28c7f3d515bf28bba
pgallazzi <pablogallazzi@gmail.com>
2016-04-20 23:25:39 -0300

    Removes unused imports

--- src/main/java/spring/Application.java
+++ src/main/java/spring/Application.java
@@ -2,8 +2,6 @@ package spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 
 /**
  * Created by pgallazzi on 31/3/16.
