83dcf74bdb85ea338bb367799ae4831a480a632d
pgallazzi <pablogallazzi@gmail.com>
2016-04-20 23:27:33 -0300

    Adds findByUserId method to UsersRepository

--- src/main/java/repositories/UsersRepository.java
+++ src/main/java/repositories/UsersRepository.java
@@ -3,6 +3,7 @@ package repositories;
 import domain.Character;
 import domain.Team;
 import domain.User;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Repository;
@@ -17,25 +18,29 @@ public class UsersRepository {
     @Autowired
     private DSMongoInterface ds;
 
-    public User findById(Integer id){
+    public User findById(Integer id) {
         return ds.getDatastore().find(User.class, "userId", id).get();
     }
 
-    public User findByUserNameAndPassword(String userName, String password){
+    public User findByUserNameAndPassword(String userName, String password) {
         User aUser = ds.getDatastore().find(User.class, "userName", userName).get();
         return aUser.passwordIsCorrect(password) ? aUser : null;
     }
 
-    public void save(User newUser){
+    public User findByUserId(String id) {
+        return ds.getDatastore().find(User.class, "userId", new ObjectId(id)).get();
+    }
+
+    public void save(User newUser) {
         ds.getDatastore().save(newUser);
     }
 
-    public void saveWithCharacter(User newUser, Character newCharacter){
+    public void saveWithCharacter(User newUser, Character newCharacter) {
         ds.getDatastore().save(newCharacter);
         ds.getDatastore().save(newUser);
     }
 
-    public void saveWithTeam(User newUser, Team newTeam){
+    public void saveWithTeam(User newUser, Team newTeam) {
         ds.getDatastore().save(newTeam);
         ds.getDatastore().save(newUser);
     }
