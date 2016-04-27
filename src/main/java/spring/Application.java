e959955d7878048aa94387bff686f36776e691bc
niko118 <nicolas.garcia@hotmail.com.ar>
2016-04-27 19:25:45 -0300

    Adding configuration for build war: 'mvn package' works now.

--- src/main/java/spring/Application.java
+++ src/main/java/spring/Application.java
@@ -2,13 +2,20 @@ package spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 /**
  * Created by pgallazzi on 31/3/16.
  */
 @SpringBootApplication(scanBasePackages = {"services", "spring", "repositories"})
-public class Application {
-    public static void main(String[] args) {
+public class Application extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+
+    public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
     }
 }
