f8eef1951901c0eabc97b39ea4dbca3353842750
Ivan <ivan.luetich93@gmail.com>
2016-05-11 22:53:01 -0300

    Reorganizing code in favoritesController.

--- src/main/webapp/app/controllers/favorites_controller.js
+++ src/main/webapp/app/controllers/favorites_controller.js
@@ -4,15 +4,24 @@
 'use strict';
 app.controller('favoritesController', ['$scope', '$location', '$cookieStore', 'favoritesService', function($scope, $location, $cookieStore, favoritesService) {
 
-    $scope.token = $cookieStore.get('access_token');
     obtainFavorites();
 
     function obtainFavorites() {
-        var token = $scope.token;
-        var delimiterPosition = token.indexOf("-");
-        var userId = token.slice(0, delimiterPosition);
+        var token = $cookieStore.get('access_token');
+        var userId = getUserIdBy(token);
         favoritesService.getFavoritesByToken(userId, token).success(function(favorites) {
-            $scope.favorites = favorites;
+            $scope.favorites = favorites.map(function(character) {
+                character.image_path = getImagePathFor(character);
+                return character;
+            })
         });
+    }
+
+    function getImagePathFor(character) {
+        return character.thumbnail.path + "." + character.thumbnail.extension;
+    }
+
+    function getUserIdBy(token) {
+        return token.slice(0, token.indexOf("-"));
     }
 }]);
