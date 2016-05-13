8567f57aff473d2faf749afc989d589945434d0f
pgallazzi <pablogallazzi@gmail.com>
2016-05-13 17:31:50 -0300

    Adds openshift configurations and scripts

--- src/main/java/services/DSMongoReal.java
+++ src/main/java/services/DSMongoReal.java
@@ -1,20 +1,16 @@
 package services;
 
-import com.mongodb.Mongo;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoCredential;
 import com.mongodb.ServerAddress;
 import de.bwaldvogel.mongo.MongoServer;
-import de.bwaldvogel.mongo.backend.memory.MemoryBackend;
 import org.mongodb.morphia.Datastore;
 import org.mongodb.morphia.Morphia;
-import org.mongodb.morphia.converters.IntegerConverter;
 import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
-import java.net.InetSocketAddress;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.List;
@@ -37,16 +33,17 @@ public class DSMongoReal implements DSMongoInterface {
     public void initialize() throws UnknownHostException {
         String host = System.getenv("OPENSHIFT_MONGODB_DB_HOST");
         String port = System.getenv("OPENSHIFT_MONGODB_DB_PORT");
-        Integer port_number = Integer.getInteger(port);
+        Integer port_number = new Integer(port);
         String user = System.getenv("OPENSHIFT_MONGODB_DB_USERNAME");
         String pass = System.getenv("OPENSHIFT_MONGODB_DB_PASSWORD");
+        String gearName = System.getenv("OPENSHIFT_GEAR_NAME");
         ServerAddress addr = new ServerAddress(host, port_number);
         List<MongoCredential> credencials = new ArrayList<MongoCredential>();
-        MongoCredential cred = MongoCredential.createCredential(user, "tptacs", pass.toCharArray());
+        MongoCredential cred = MongoCredential.createCredential(user, gearName, pass.toCharArray());
         credencials.add(cred);
         Morphia morphia = new Morphia();
         morphia.mapPackage("domain");
-        ds = morphia.createDatastore(new MongoClient(addr,credencials),"tptacs");
+        ds = morphia.createDatastore(new MongoClient(addr,credencials),gearName);
         ds.ensureIndexes();
     }
 
