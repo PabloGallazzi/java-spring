c143b0d2a56e1be31880d73e36060af14608b166
pgallazzi <pablogallazzi@gmail.com>
2016-05-14 20:01:49 -0300

    travis

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,8 @@ if [[ ! $TRAVIS_BRANCH =~ ^master.*$ ]]; then
   exit 1
 fi
 
+gem install rhc
+rhc apps --token 932b2746d50b40dec2704669d07e488f315e2df9373212b8a32e0883b389baaa
 git remote add openshift -f ssh://5737931a7628e17321000043@tptacsutnfrba-pablogallazzi.rhcloud.com/~/git/tptacsutnfrba.git/
 git merge openshift/master -s recursive -X ours -m 'merge'
 git push openshift HEAD
