3da0901e1bfe26457e6f90cc2ef67879bc4833f0
pgallazzi <pablogallazzi@gmail.com>
2016-05-14 20:47:37 -0300

    script to deploy

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,13 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-if [[ $TRAVIS_PULL_REQUEST != "false" ]]; then
-  echo "Skip deployment - Deployment doesn't run on pull requests"
-  exit 1
-fi
-
-if [[ ! $TRAVIS_BRANCH =~ ^master.*$ ]]; then
-  echo "Skip deployment - Deployment runs only on release branch"
-  exit 1
-fi
-
-git clone ssh://5737931a7628e17321000043@tptacsutnfrba-pablogallazzi.rhcloud.com/~/git/tptacsutnfrba.git/
-gem install rhc
-rhc apps --token 932b2746d50b40dec2704669d07e488f315e2df9373212b8a32e0883b389baaa
+mkdir deploy
+cd deploy
+git clone https://github.com/niko118/tp-tacs.git
+cd tp-tacs
 git remote add openshift -f ssh://5737931a7628e17321000043@tptacsutnfrba-pablogallazzi.rhcloud.com/~/git/tptacsutnfrba.git/
 git merge openshift/master -s recursive -X ours -m 'merge'
 git push openshift HEAD
-
+cd ..
+cd ..
+rm -rf deploy
 exit 0
