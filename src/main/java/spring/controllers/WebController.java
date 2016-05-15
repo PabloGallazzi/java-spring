1ff6b716613ba9dff5d77265e9ca271b6ce08660
Damian Adan <adan.damian@gmail.com>
2016-05-15 16:05:59 -0300

    Pagina de error

--- src/main/java/spring/controllers/WebController.java
+++ src/main/java/spring/controllers/WebController.java
@@ -1,14 +1,10 @@
 package spring.controllers;
 
 import domain.Token;
-import org.apache.catalina.connector.Request;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import repositories.AuthRepository;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletRequestWrapper;
 import javax.servlet.http.HttpServletResponse;
 
 /**
@@ -23,14 +19,21 @@ public class WebController {
     @RequestMapping(value = "/home", method = RequestMethod.GET)
     public String login(HttpServletResponse httpServletResponse,
                         @CookieValue(value = "access_token", required = true, defaultValue = "") String accessToken) {
-        Token.validateNonEmptyToken(accessToken);
-        Token aToken = auth.findById(accessToken);
-        httpServletResponse.setStatus(200);
+        try
+        {
+            Token.validateNonEmptyToken(accessToken);
+            Token aToken = auth.findById(accessToken);
+            httpServletResponse.setStatus(200);
 
-        if (aToken.isAdmin()) {
-            return "home_admin";
-        } else {
-            return "home_user";
+            if (aToken.isAdmin()) {
+                return "home_admin";
+            } else {
+                return "home_user";
+            }
+        }
+        catch (Exception ex)
+        {
+            return "error";
         }
     }
 }
