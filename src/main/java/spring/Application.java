8a229dbc9e336cadcd66ae026cc3369938c1a072
pgallazzi <pablogallazzi@gmail.com>
2016-05-22 19:48:23 -0300

    Removes unnecessary stuff

--- src/main/java/spring/Application.java
+++ src/main/java/spring/Application.java
@@ -3,24 +3,26 @@ package spring;
 import org.apache.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.ApplicationContext;
+
+import java.util.Arrays;
 
 /**
  * Created by pgallazzi on 31/3/16.
  */
 @SpringBootApplication(scanBasePackages = {"services", "spring", "repositories"})
-public class Application extends SpringBootServletInitializer {
+public class Application {
 
     private static final Logger logger = Logger.getLogger(Application.class);
 
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(Application.class);
-    }
-
     public static void main(String[] args) throws Exception {
-        SpringApplication.run(Application.class, args);
+        ApplicationContext ctx = SpringApplication.run(Application.class, args);
         logger.info("The application is up!");
+        logger.info("Let's inspect the beans provided by Spring Boot:");
+        String[] beanNames = ctx.getBeanDefinitionNames();
+        Arrays.sort(beanNames);
+        for (String beanName : beanNames) {
+            logger.info(beanName);
+        }
     }
 }
