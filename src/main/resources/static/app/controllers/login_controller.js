236e9aaee33ce5125149c30cbee987d31629a9ed
pgallazzi <pablogallazzi@gmail.com>
2016-05-29 13:48:26 -0300

    Bumps up cookies expiration date to be higher than the access_tokens expiration date

--- src/main/resources/static/app/controllers/login_controller.js
+++ src/main/resources/static/app/controllers/login_controller.js
@@ -1,25 +1,25 @@
 app.controller('loginController', ['$scope', '$location', 'userService', 'errorService',
-    function($scope, $location, userService, errorService) {
+    function ($scope, $location, userService, errorService) {
 
-    $scope.userName = null;
-    $scope.userPassword = null;
-    $scope.errors = {};
-    $scope.showError = errorService.showApiError;
+        $scope.userName = null;
+        $scope.userPassword = null;
+        $scope.errors = {};
+        $scope.showError = errorService.showApiError;
 
-    $scope.login = function() {
+        $scope.login = function () {
 
-        var username = $scope.userName;
-        var password = $scope.userPassword;
-        userService.login(username, password)
-            .success(function(user) {
-                setCookie('access_token',user.access_token,1);
-                userService.redirectHome();
-            })
-            .error(function (error) {
-                console.error('Error while authenticating user');
-                $scope.errors.api = error.error;
-            })
-        ;
-    };
+            var username = $scope.userName;
+            var password = $scope.userPassword;
+            userService.login(username, password)
+                .success(function (user) {
+                    setCookie('access_token', user.access_token, 190);
+                    userService.redirectHome();
+                })
+                .error(function (error) {
+                    console.error('Error while authenticating user');
+                    $scope.errors.api = error.error;
+                })
+            ;
+        };
 
-}]);
+    }]);
