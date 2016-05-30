ff48ee7e80ce260fdc0798b1cc2e8485200a3f27
Iván Luetich <iluetich@users.noreply.github.com>
2016-05-30 15:52:05 -0300

    Adding promises handlers to favoriteService

--- src/main/resources/static/app/services/favorites_service.js
+++ src/main/resources/static/app/services/favorites_service.js
@@ -18,13 +18,23 @@ app.factory('favoritesService', ['$http', '$q', function ($http, $q) {
         var params = {access_token: token};
         var config = {params: params, headers: {'Content-Type': 'application/json;charset=utf-8;'}};
         var url = '/users/' + user_id + '/characters/favorites';
-        return $http.get(url, config);
+        return $http.get(url, config).then(function(response) {
+            return response.data;
+        }, function(errResponse) {
+            console.error('Error while getting favorites.');
+            return $q.reject(errResponse);
+        });
     }
 
     function addFavorite(user_id, token, character) {
         var config = {headers: {'Content-Type': 'application/json;charset=utf-8;'}};
         var url = '/users/' + user_id + '/characters/favorites?access_token=' + token;
-        return $http.post(url, character, config);
+        return $http.post(url, character, config).then(function(response) {
+            return response.data;
+        }, function(errResponse) {
+            console.error('Character: ' + character.name + ' could not be added to favorites.')
+            $q.reject(errResponse);
+        });
     }
 
     function removeFavorite(user_id, token, character) {
@@ -38,7 +48,12 @@ app.factory('favoritesService', ['$http', '$q', function ($http, $q) {
         var params = {access_token: token);
         var config = {params: params, headers: {'Content-Type': 'application/json;charset=utf-8;'}};
         var url = 'users/' + user_id + '/characters/favorites/' + character.id;
-        return $http.get(url, config);
+        return $http.get(url, config).then(function(response) {
+            return response.data;
+        }, function(errResponse) {
+            console.error('Could not determine if character: ' + character.name + 'is a favorite.');
+            $q.reject(errResponse);
+        });
     }
 
 
