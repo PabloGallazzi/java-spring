e03ced9015139f1d5a1fa5eb34bac2978e86d699
Ivan <ivan.luetich93@gmail.com>
2016-06-07 23:33:32 -0300

    Adding notification on success/empty intersection.

--- src/main/resources/static/app/controllers/commons_controller.js
+++ src/main/resources/static/app/controllers/commons_controller.js
@@ -2,7 +2,7 @@
  * Created by ivan on 04/06/16.
  */
 app.controller('commonsController', ['$scope', 'userService', function ($scope, userService) {
-    
+
     $scope.users = [];
     $scope.teams = [];
     $scope.characters = [];
@@ -10,15 +10,21 @@ app.controller('commonsController', ['$scope', 'userService', function ($scope, 
     $scope.user_2 = '';
     $scope.team_1 = '';
     $scope.team_2 = '';
+    $scope.intersected = false;
 
     $scope.getIntersection = getIntersection;
     $scope.getTeams = getTeams;
     $scope.getUsers = getUsers;
+    $scope.showResult = showResult;
+    $scope.canDoIntersection = function () {
+        return $scope.team_1 && $scope.team_2 && !$scope.intersected
+    };
 
     function getIntersection() {
         var id1 = $scope.team_1.team_id;
         var id2 = $scope.team_2.team_id;
         userService.getTeamsIntersection(id1, id2).then(function (characters) {
+            $scope.intersected = true;
             $scope.characters = characters;
         }, function (err) {
             console.error(err);
@@ -34,11 +40,18 @@ app.controller('commonsController', ['$scope', 'userService', function ($scope, 
     }
 
     function getTeams(user) {
+        $scope.intersected = false;
         userService.getUserTeams(user.user_id).then(function (response) {
             $scope.teams[user.user_name] = response.data;
         }, function (err) {
             console.error(err);
         })
+    }
+    
+    function showResult() {
+        var onTrue = "Check the characters in common!";
+        var onFalse = "The teams: " + $scope.team_1.team_name + " and " + $scope.team_2.team_name + " have no characters in common. Try again changing teams!";
+        return $scope.characters.length ? onTrue : onFalse;
     }
 
 }]);
