51b17db9c40e66dc74a381bf4df665819eb7f3c2
Ian Cincotta <icincotta@araricincotta.ml.com>
2016-06-16 00:34:02 -0300

    luego de registrarme me logueo

--- src/main/resources/static/app/controllers/register_controller.js
+++ src/main/resources/static/app/controllers/register_controller.js
@@ -12,8 +12,20 @@ app.controller('registerController', ['$scope', '$location', 'userService', 'err
         var username = $scope.username;
         var password = $scope.password;
         userService.register(username, password)
-            .success(userService.redirectHome)
-            .error(function (error) {
+            .success(function(){
+                userService.login(username, password)
+                    .success(function (user) {
+                        setCookie('access_token', user.access_token, 190);
+                        setCookie('nick_name', username, 190);
+                        userService.redirectHome();
+                    })
+                    .error(function (error) {
+                        console.error('Error while authenticating user');
+                        $scope.errors.api = error.error;
+                    })
+                ;
+            })
+            .error(function(error){
                 console.error('Error while creating user account');
                 $scope.errors.api = error.cause;
             })
