80b8ddac456445b520430db8d3e2756b9aa3e62e
Pablo Gallazzi <PabloGallazzi@users.noreply.github.com>
2016-12-24 19:39:40 -0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 mkdir deploy
 cd deploy
-git clone https://github.com/niko118/tp-tacs.git
+git clone https://github.com/PabloGallazzi/java-spring.git
 cd tp-tacs
 git remote add openshift -f ssh://5737931a7628e17321000043@tptacsutnfrba-pablogallazzi.rhcloud.com/~/git/tptacsutnfrba.git/
 git merge openshift/master -s recursive -X ours -m 'merge'
